68687239e82182b47bdf4aab08adef501291c43a
iminvisk <iminvisk@gmail.com>
2016-05-18 20:02:40 +0700

    Database updated

--- database/migrations/2016_05_17_132035_create_location_types_table.php
+++ database/migrations/2016_05_17_132035_create_location_types_table.php
@@ -14,6 +14,7 @@ class CreateLocationTypesTable extends Migration
     {
         Schema::create('location_types', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('key');
             $table->string('name');
             $table->timestamps();
         });
