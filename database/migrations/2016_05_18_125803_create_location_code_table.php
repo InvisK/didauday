cb74d81c178821b9ed4bc7f043c2657fbfed6acb
iminvisk <iminvisk@gmail.com>
2016-05-18 20:02:54 +0700

    Database updated

---
+++ database/migrations/2016_05_18_125803_create_location_code_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLocationCodeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('location_code', function (Blueprint $table) {
+            $table->integer('id');
+            $table->integer('location_news_id');
+            $table->string('promo_value');
+            $table->integer('promo_unit_id');
+            $table->string('keyword');
+            $table->timestamps();
+        });
+    }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('location_code');
+    }
+}
